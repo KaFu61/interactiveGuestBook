7c4a7bd19d6461ca4b87164d991ae857ae57ad3d
KaFu61 <kathrin.fuhrer@gmail.com>
2016-07-19 11:14:24 +0200

    getAverage

--- Assets/Scripts/DepthData.cs
+++ Assets/Scripts/DepthData.cs
@@ -11,16 +11,16 @@ public class DepthData : MonoBehaviour {
 
 	private ushort[] depths;
 
-	private int width = 512;
-	private int height = 424;
+	private ushort width = 512;
+	private ushort height = 424;
 
 	//Angabe in mm
-	private float screen_depth = 0;
-	private byte mintreshold = 70;
-	private byte maxtreshold = 100;
+	private ushort screen_depth = 0;
+	private byte mintreshold = 50;
+	private byte maxtreshold = 150;
 	private ushort right_side_limit = 498;
 
-	private ushort big_difference = 3;
+	private byte big_difference = 3;
 
 	private int secondFrame = 0;
 
@@ -28,15 +28,19 @@ public class DepthData : MonoBehaviour {
 
 	// 1 -> x, 2 -> y, 3 -> minval
 	private List<Vector3> points_to_draw = new List<Vector3>();
-	private int max_points_to_draw = 300;
+	private int max_points_to_draw = 1;
+
+	private Vector3 current_minval;
 
 	//Create new GameObject (Circle)
 	public GameObject the_circle;
 	public GameObject instance;
 
+	private float z = 900;
+
 	//Canvas Action
 	public GameObject canvas;
-	private int wait_til_draw;
+	private ushort wait_til_draw;
 	private int frames_to_wait_canvas = 0;
 
 	private int waiting_frames = 50;
@@ -60,9 +64,14 @@ public class DepthData : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update() {
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+			Application.Quit();
+		}
 		float minval = 10000;
 		int minval_x = 0;
 		int minval_y = 0;
+
+		current_minval = new Vector3(10000, 10000, -5);
 
 		//delete lists content to produce a new one for this frame
 		points_to_draw.Clear();
@@ -75,8 +84,8 @@ public class DepthData : MonoBehaviour {
 		depths = depth_manager.GetDepthData();
 
 		if (wait_til_draw == 180) {
-			screen_depth = (depths[72397] + depths[72499] + depths[108800] + depths[144589] + depths[144691]) / 5;
-			Debug.Log(screen_depth);
+			screen_depth = getScreenDepth();
+			Debug.Log("screen_depth: " + screen_depth);
 			wait_til_draw = 10000;
 		} else if (wait_til_draw < 180) {
 			wait_til_draw++;
@@ -91,49 +100,68 @@ public class DepthData : MonoBehaviour {
 			return;
 		}
 
-		for (int x = 0; x < width; x = x + 1) {
-			for (int y = 0; y < height; y = y + 1) {
-				if (depths[y * width + x] < (screen_depth - mintreshold) && depths[y * width + x] < minval && depths[y * width + x] != 0 && isInAllowedRange(x, y)) {
-						
-					if (canvas.activeSelf == false && (depths[y * width + x] > screen_depth - maxtreshold)) {
+		if (secondFrame == 0) {
+			for (int x = 0; x < width; x = x + 1) {
+				for (int y = 0; y < height; y = y + 1) {
+					if (depths[y * width + x] < (screen_depth - mintreshold) && depths[y * width + x] < minval && depths[y * width + x] != 0 && isInAllowedRange(x, y)) {
 
-						if (x > right_side_limit) {
-							canvas.SetActive(true);
-							frames_to_wait_canvas = waiting_frames;
+						if (canvas.activeSelf == false && (depths[y * width + x] > screen_depth - maxtreshold)) {
+
+							if (x > right_side_limit) {
+								canvas.SetActive(true);
+								frames_to_wait_canvas = waiting_frames;
+							}
+
+							//check the depths of the pixel next to the current to reduce voice
+							if (x < right_side_limit) {
+								minval = depths[y * width + x];
+
+								minval_x = x;
+								minval_y = y;
+
+								current_minval = new Vector3(minval_x, minval_y, minval);
+								//getPointsToDraw(current_min_val);
+							}
+
+						} else if (canvas.activeSelf == true) {
+							if (x > width - right_side_limit && x < right_side_limit) {
+								color = tex.GetPixel(x, y);
+								canvas.SetActive(false);
+								frames_to_wait_canvas = waiting_frames;
+							} else if (x < width - right_side_limit) {
+								canvas.SetActive(false);
+								frames_to_wait_canvas = waiting_frames;
+							}
 						}
 
-						//check the depths of the pixel next to the current to reduce voice
-						if (x < right_side_limit) {
-							minval = depths[y * width + x];
-
-							minval_x = x;
-							minval_y = y;
-
-							Vector3 current_min_val = new Vector3(minval_x, minval_y, minval);
-							getPointsToDraw(current_min_val);
-						}
-
-					} else if (canvas.activeSelf == true) {
-						if (x > width - right_side_limit && x < right_side_limit) {
-							color = tex.GetPixel(x, y);
-							canvas.SetActive(false);
-							frames_to_wait_canvas = waiting_frames;
-						} else if (x < width - right_side_limit) {
-							canvas.SetActive(false);
-							frames_to_wait_canvas = waiting_frames;
-						}
 					}
-						
-				}
 
+				}
 			}
-		}
-		if (frames_to_wait_canvas == 0) {
-			foreach (Vector3 point in points_to_draw) {
-				instance = (GameObject)Instantiate(the_circle, new Vector3(point.x, point.y, 1), transform.rotation);
+
+			if (frames_to_wait_canvas == 0) {
+				instance = (GameObject)Instantiate(the_circle, new Vector3(current_minval.x, current_minval.y, z), transform.rotation);
 				instance.GetComponent<Renderer>().material.color = color;
+				byte point_size = (byte)(1+ (screen_depth - mintreshold - current_minval.z) * 2 / 10);
+				instance.GetComponent<Renderer>().transform.localScale = new Vector3(point_size, point_size, 1);
+				z = z - 0.001f;
+				/*
+				foreach (Vector3 point in points_to_draw) {
+					instance = (GameObject)Instantiate(the_circle, new Vector3(point.x, point.y, z), transform.rotation);
+					instance.GetComponent<Renderer>().material.color = color;
+					byte point_size = (byte) ((screen_depth - mintreshold - point.z) * 3 / 8);
+					instance.GetComponent<Renderer>().transform.localScale = new Vector3(point_size, point_size, 1);
+					z++;
+				}
+				*/
 			}
+			secondFrame = 1;
+		} else {
+			secondFrame = 0;
 		}
+
+
+		
 		
 		//Debug.Log(minval + "-" + minval_x + "-" + minval_y);
 	}
@@ -188,5 +216,19 @@ public class DepthData : MonoBehaviour {
 				points_to_draw.RemoveAt(max_points_to_draw);
 			}
 		}	
+	}
+
+	public ushort getScreenDepth() {
+		uint depth = 0;
+		uint cntr = 0;
+
+		for (int x = 0; x < width; x = x + 1) {
+			for (int y = 0; y < height; y = y + 1) {
+				depth += depths[y * width + x];
+				cntr++;
+			}
+		}
+
+		return (ushort) (depth / cntr);
 	}
 }
